da8c4eb7fb1ddd4a02d9285f867b60bcde3a58a8
Leon <yanglyu.leon.7@gmail.com>
2017-02-22 15:15:14 -0800

    Remove my-new-component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,10 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { MyNewComponentComponent } from './my-new-component/my-new-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MyNewComponentComponent
   ],
   imports: [
     BrowserModule,
